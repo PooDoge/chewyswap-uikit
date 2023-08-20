301e346a4f998083a10108914dba1a89f73f86dd
Jibmo <dev@poodoge.net>
2023-08-20 15:39:19 -0400

    Change back lol

--- src/widgets/Menu/Logo.tsx
+++ src/widgets/Menu/Logo.tsx
@@ -23,8 +23,7 @@ const StyledLink = styled(Link)`
     }
   }
   .desktop-icon {
-    width: 150px;
-    height: 64px;
+    width: 156px;
     display: none;
     ${({ theme }) => theme.mediaQueries.nav} {
       display: block;
